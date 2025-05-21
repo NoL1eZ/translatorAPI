cb1a7347ab216e033bdefbc2c2f817012080a7f1
NoL1eZ <108579116+NoL1eZ@users.noreply.github.com>
2025-05-21 19:53:04 +0300

    migration

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.routers import assignments, substitution, title, translator
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "My e-commerce app"}
+
+
+app.include_router(assignments.router)
+app.include_router(substitution.router)
+app.include_router(title.router)
+app.include_router(translator.router)
